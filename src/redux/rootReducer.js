fcfbf59706f6b617a3d9bafe3e53f75e31c2aa5b
Aidar Ibatullin <amazing.space.invader@gmail.com>
2018-01-24 18:28:50 +0500

    Closes #1 Create auth layer Closes #2 Create SignIn container. Create initSignIn and verifySignIn forms Closes #3 Create SignUp container. Create initSignUp and verifySignUp forms Closes #4 Create RecoveryPassword container. Create initRecoveryPassword, verifyRecoveryPassword and setNewPassword forms

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,8 +1,6 @@
 import { combineReducers, routerReducer as routing } from 'redux-seamless-immutable';
 import { reducer as form } from 'redux-form';
 
-import app from './modules/app/app';
-
 import signInReducer from './modules/auth/signIn';
 import signUpReducer from './modules/auth/signUp';
 import recoveryPasswordReducer from './modules/auth/recoveryPassword';
@@ -10,10 +8,6 @@ import recoveryPasswordReducer from './modules/auth/recoveryPassword';
 export default combineReducers({
   routing,
   form,
-
-  app: combineReducers({
-    app
-  }),
 
   auth: combineReducers({
     signIn: signInReducer,
--- src/sagas/rootSaga.js
+++ src/sagas/rootSaga.js
@@ -1,11 +1,8 @@
 import { all, fork } from 'redux-saga/effects';
 import { formActionSaga } from 'redux-form-saga';
 
-import appSaga from './app/app';
-
 export default function* () {
   yield all([
-    fork(formActionSaga),
-    fork(appSaga)
+    fork(formActionSaga)
   ]);
 }
