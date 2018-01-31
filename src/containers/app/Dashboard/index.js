75e195ce7f4ed2ba0191b61f54b7fd5f1654a10e
Aidar Ibatullin <amazing.space.invader@gmail.com>
2018-01-31 20:55:30 +0500

    Closes #33. Update balances

--- src/containers/app/Dashboard/index.js
+++ src/containers/app/Dashboard/index.js
@@ -17,11 +17,19 @@ class Dashboard extends Component {
   render() {
     const {
       ethBalance,
-      erc20TokenBalance,
+      lbrsBalance,
+      erc20TokensBalance,
       ethAddress,
       email,
       name
     } = this.props;
+
+    const renderTableRow = (token) => (
+      <tr key={token.contractAddress}>
+        <td>{token.symbol}</td>
+        <td>{token.balance}</td>
+      </tr>
+    );
 
     return (
       <div className="animated fadeIn mt-4">
@@ -47,8 +55,8 @@ class Dashboard extends Component {
 
             <Card className="text-white bg-success">
               <CardBody>
-                <h2 className="mb-2">{erc20TokenBalance}</h2>
-                <h5 className="mb-2">ERC20 token wallet</h5>
+                <h2 className="mb-2">{lbrsBalance}</h2>
+                <h5 className="mb-2">LBRS wallet</h5>
                 <p>{ethAddress}</p>
                 <a className="btn btn-secondary disabled"><i className="icon-magic-wand"></i>&nbsp;&nbsp;Transfer</a>{' '}
                 <button className="btn btn-secondary text-white" disabled><i className="icon-wallet"></i>&nbsp;&nbsp;Copy address</button>
@@ -56,39 +64,23 @@ class Dashboard extends Component {
             </Card>
           </Col>
 
-          <Col xs="12" lg="3"></Col>
-
-          <Col xs="12" lg="4">
-            <RegisterToken/>
-
+          <Col xs="12" lg="3">
             <Card className="bg-white">
               <CardHeader>
                 <h4 className="my-0">Balances</h4>
-                <p className="mt-1 mb-0">This module disabled now</p>
               </CardHeader>
               <CardBody>
                 <Table responsive>
                   <tbody>
-                  <tr>
-                    <td>ETH</td>
-                    <td className="text-success">1.23388485 (+5%)</td>
-                  </tr>
-                  <tr>
-                    <td>ERC20</td>
-                    <td className="text-success">29494.23388485 (+15%)</td>
-                  </tr>
-                  <tr>
-                    <td>USD</td>
-                    <td className="text-danger">800 (-5%)</td>
-                  </tr>
-                  <tr>
-                    <td>RUB</td>
-                    <td className="text-danger">10030 (-55%)</td>
-                  </tr>
+                  {erc20TokensBalance.map((token) => renderTableRow(token))}
                   </tbody>
                 </Table>
               </CardBody>
             </Card>
+          </Col>
+
+          <Col xs="12" lg="4">
+            <RegisterToken/>
           </Col>
         </Row>
       </div>
