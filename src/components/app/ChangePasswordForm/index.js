3c6e16ff9dc1570254c7251bc1fbde0e91aaa1ec
Aidar Ibatullin <amazing.space.invader@gmail.com>
2018-02-05 09:15:31 +0500

    Closes #47. Passwords tips

--- src/components/app/ChangePasswordForm/index.js
+++ src/components/app/ChangePasswordForm/index.js
@@ -45,6 +45,12 @@ const ChangePasswordForm = (props) => {
           validate={passwordValidate}/>
       </FormGroup>
 
+      <Alert color="info">
+        Password can contain lowercase and uppercase letters, numbers and special characters&emsp;
+        {// eslint-disable-next-line
+        }<code>&#33;&#34;&#35;&#36;&#37;&#38;&#39;&#40;&#41;&#42;&#43;&#58;&#59;&#60;&#61;&#62;&#63;&#64;&#91;&#93;&#94;&#95;&#96;&#123;&#124;&#125;&#126;</code>
+      </Alert>
+
       {error ? <Alert color="danger">{error}</Alert> : null}
 
       <FormGroup>
--- src/components/auth/SetNewPasswordForm/index.js
+++ src/components/auth/SetNewPasswordForm/index.js
@@ -42,6 +42,12 @@ const SetNewPasswordForm = (props) => {
         name="resetId"
         type="hidden"/>
 
+      <Alert color="info">
+        Password can contain lowercase and uppercase letters, numbers and special characters&emsp;
+        {// eslint-disable-next-line
+        }<code>&#33;&#34;&#35;&#36;&#37;&#38;&#39;&#40;&#41;&#42;&#43;&#58;&#59;&#60;&#61;&#62;&#63;&#64;&#91;&#93;&#94;&#95;&#96;&#123;&#124;&#125;&#126;</code>
+      </Alert>
+
       {error ? <Alert color="danger">{error}</Alert> : null}
 
       {renderButton()}
