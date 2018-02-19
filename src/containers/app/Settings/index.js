2c29e6f70a9e7ee5427b9b00988d26cd965187bc
Aidar Ibatullin <amazing.space.invader@gmail.com>
2018-02-19 20:28:39 +0800

    Closes #76

--- src/containers/app/Settings/index.js
+++ src/containers/app/Settings/index.js
@@ -1,73 +1,80 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Row, Col, Card, CardBody, CardHeader } from 'reactstrap';
 
 import { initChangePassword, verifyChangePassword } from '../../../redux/modules/app/changePassword';
 import { setNotifications } from '../../../redux/modules/app/manageEmailNotifications';
+import { fetchUser } from '../../../redux/modules/app/app';
 
 import ChangePasswordForm from '../../../components/app/ChangePasswordForm';
 import VerifyChangePasswordForm from '../../../components/app/VerifyTransferTokensForm';
 import ManageEmailNotificationsForm from '../../../components/app/ManageEmailNotificationsForm';
 
-const Settings = (props) => {
-  const {
-    changePasswordStep,
-    changePasswordFetching,
-    changePasswordVerification,
-    manageEmailNotificationsFetching,
-    notifications
-  } = props;
+class Settings extends Component {
+  componentDidMount() {
+    this.props.fetchUser();
+  }
 
-  const renderStep = (currentStep) => {
-    switch (currentStep) {
-      case 'changePassword':
-        return (
-          <ChangePasswordForm
-            onSubmit={initChangePassword}
-            fetching={changePasswordFetching}/>
-        );
-      case 'verifyChangePassword':
-        return (
-          <VerifyChangePasswordForm
-            onSubmit={verifyChangePassword}
-            fetching={changePasswordFetching}
-            initialValues={{
-              verification: {
-                verificationId: changePasswordVerification.verificationId
-              }
-            }}/>
-        );
-      default:
-        return 'Something went wrong';
-    }
-  };
+  render() {
+    const {
+      changePasswordStep,
+      changePasswordFetching,
+      changePasswordVerification,
+      manageEmailNotificationsFetching,
+      notifications
+    } = this.props;
 
-  return (
-    <div className="animated fadeIn mt-4">
-      <Row>
-        <Col xs="12" lg="4">
-          <Card>
-            <CardHeader>Change password</CardHeader>
-            <CardBody>
-              {renderStep(changePasswordStep)}
-            </CardBody>
-          </Card>
-        </Col>
-        <Col xs="12" lg="4">
-          <Card>
-            <CardHeader>Manage e-mail notifications</CardHeader>
-            <CardBody>
-              <ManageEmailNotificationsForm
-                onSubmit={setNotifications}
-                fetching={manageEmailNotificationsFetching}
-                initialValues={{ ...notifications }}/>
-            </CardBody>
-          </Card>
-        </Col>
-      </Row>
-    </div>
-  );
-};
+    const renderStep = (currentStep) => {
+      switch (currentStep) {
+        case 'changePassword':
+          return (
+            <ChangePasswordForm
+              onSubmit={initChangePassword}
+              fetching={changePasswordFetching}/>
+          );
+        case 'verifyChangePassword':
+          return (
+            <VerifyChangePasswordForm
+              onSubmit={verifyChangePassword}
+              fetching={changePasswordFetching}
+              initialValues={{
+                verification: {
+                  verificationId: changePasswordVerification.verificationId
+                }
+              }}/>
+          );
+        default:
+          return 'Something went wrong';
+      }
+    };
+
+    return (
+      <div className="animated fadeIn mt-4">
+        <Row>
+          <Col xs="12" lg="4">
+            <Card>
+              <CardHeader>Change password</CardHeader>
+              <CardBody>
+                {renderStep(changePasswordStep)}
+              </CardBody>
+            </Card>
+          </Col>
+          <Col xs="12" lg="4">
+            <Card>
+              <CardHeader>Manage e-mail notifications</CardHeader>
+              <CardBody>
+                <ManageEmailNotificationsForm
+                  onSubmit={setNotifications}
+                  fetching={manageEmailNotificationsFetching}
+                  initialValues={{ ...notifications }}/>
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
+      </div>
+    );
+  }
+}
 
 export default connect(
   (state) => ({
@@ -77,5 +84,7 @@ export default connect(
     manageEmailNotificationsFetching: state.app.manageEmailNotifications.fetching,
     notifications: state.app.app.user.preferences.notifications
   }),
-  {}
+  {
+    fetchUser
+  }
 )(Settings);
